b152d711f9dbcedac1804062f59383cdf7481a7f
CDAGaming <chrisdoesawesomness@gmail.com>
2018-01-16 23:04:38 -0600

    Upgrades and Branch Initialization

--- .travis/linux/deps.sh
+++ .travis/linux/deps.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -ex
 
 docker pull ubuntu:18.04
+sudo apt-get remove -y postgresql-9.1
+sudo apt-get remove -y postgresql-9.2
+sudo apt-get remove -y postgresql-9.3
+sudo apt-get remove -y postgresql-9.4
+sudo apt-get remove -y postgresql-9.5
+sudo apt-get update -q
+sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
 
 brew update
+brew upgrade
 brew install dylibbundler p7zip qt5 sdl2
 brew outdated cmake || brew upgrade cmake
